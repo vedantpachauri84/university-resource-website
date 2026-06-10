769cf380bea5d599d9f18596272a72b59fbdd99f
vedant <vedantpachauri@gmail.com>
2026-06-10 13:00:13 +0530

    Updated project with latest changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Paper, Notes, Resources, Contact
+from .models import Paper, Notes, Resources, Contact,Blog
 
 # Register your models here.
 admin.site.register(Paper)
 admin.site.register(Notes)
 admin.site.register(Resources)
 admin.site.register(Contact)
+admin.site.register(Blog)
