eb952b3330d01eea1c0c2f3cac36792fd92d249c
vedant <vedantpachauri@gmail.com>
2026-06-10 18:48:01 +0530

    fix: add cloudinary.config() for file upload authorization

--- blogs/settings.py
+++ blogs/settings.py
@@ -143,3 +143,8 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+cloudinary.config(
+    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key = os.getenv('CLOUDINARY_API_KEY'),
+    api_secret = os.getenv('CLOUDINARY_API_SECRET')
+)
